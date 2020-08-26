496ad45de8aff68f254d9c89e95db39d68126b8d
Daiana A Romero <daianaar94@gmail.com>
2020-08-25 23:00:00 -0300

    mensaje

---
+++ 25-08-2020/ejer1/ejer1/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Diagnostics.CodeAnalysis;
+
+namespace ejer1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+            /*
+             Armar un programa que le pida al usuario que adivine un numero que ustedes prefijaron, 
+            si adivina le muestran un mensaje de felicitacion, caso contrario que siga participando
+             */
+            string numero;
+            Console.WriteLine("Ingrese un numero");
+            numero = Console.ReadLine();
+
+
+
+            int num;
+            num=int.Parse(numero);
+            while(num != 8)
+            {
+                Console.WriteLine("Intente nuevamente");
+                num = int.Parse(Console.ReadLine());
+
+            }
+
+            Console.WriteLine("Adivinó");
+        }
+    }
+}
